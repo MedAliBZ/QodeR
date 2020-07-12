1d53363422b3dd7078dc6c5a809c5ecd884a1226
DaliHOST <mohamedali.bouzaiene@esprit.tn>
2020-07-12 19:49:56 +0100

    added copy button + disable for copy and download

--- apigenerate.js
+++ apigenerate.js
@@ -6,6 +6,10 @@ let QRsrc = document.querySelector('#QRcode[src]')
 
 codethisB.addEventListener("click", function change() {
     QRsrc.setAttribute("src", `http://api.qrserver.com/v1/create-qr-code/?data=${textbox.value}`)
+    copyB.removeAttribute("disabled")
+    downloadB.removeAttribute("disabled")
+    downloadB.innerHTML="Download QR code"
+    copyB.innerHTML="Copy image URL"
 })
 
 //download generate code
@@ -14,4 +18,19 @@ let downloadB = document.getElementById("download")
 
 downloadB.addEventListener("click",() => {
     saveAs(QRsrc.getAttribute('src'),`QodeR-code`)
+    downloadB.setAttribute("disabled","true")
+    downloadB.innerHTML="Downloaded ✔"
+})
+
+//Copy url
+
+let copyB=document.getElementById("copy")
+
+
+
+copyB.addEventListener("click",function nakel(){
+    let url=QRsrc.getAttribute('src')
+    navigator.clipboard.writeText(url)
+    copyB.setAttribute("disabled","true")
+    copyB.innerHTML="Copied ✔"
 })
